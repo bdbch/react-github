2948a4f48130ee376592abe7cf74fedb22e399b1
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2019-07-20 01:43:32 +0200

    implementation for branch hooks

--- docs/example.js
+++ docs/example.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import Repos from "./components/Repos";
 import User from "./components/User";
+import Branches from "./components/Branches";
+import Branch from "./components/Branch";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Repos />
       <hr />
       <User />
+      <hr />
+      <Branches />
+      <hr />
+      <Branch />
       <hr />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import useRepos from './lib/useRepos'
 import useUser from './lib/useUser'
+import {
+  useBranches,
+} from './lib/branch/useBranches'
+import {
+  useBranch
+} from './lib/branch/useBranch'
 
 export {
   useRepos,
   useUser,
+  useBranches,
+  useBranch,
 }
 
 export default {
   useRepos,
   useUser,
+  useBranches,
+  useBranch,
 }
